ecccde5f75e2c859831504476fa8d851b4384613
BHIMA Testing <jmcameron@gmail.com>
2023-11-27 16:28:24 +0100

    Work in progress

---
+++ scripts/mysql_init.sql
@@ -0,0 +1,2 @@
+CREATE USER 'bhima'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'HISCongo2013';
+GRANT ALL PRIVILEGES ON * . * TO 'bhima'@'localhost';
--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Start redis
+systemctl enable redis-server
+service redis-server start
+service redis-server status
+
 # Start MySQL
 systemctl enable mysql
 chown mysql.mysql /var/mysql
@@ -7,8 +12,8 @@ mkdir -p /var/mysql/mysqld >& /dev/null
 mkdir -p /var/mysql/home >& /dev/null
 usermod -d /var/mysql/home mysql
 service mysql start
+service mysql status
 
-# Start redis
-systemctl enable redis-server
-service redis-server start
+# Create the bhima user in MySQL
+mysql -u root < ./scripts/mysql_init.sql
 
